23f77ab18036a6f190f813c495843cc86c64a612
Sivaprakash <sivaprakash.ramasamy@tarento.com>
2021-11-16 19:22:19 +0530

    mongo + redis config

--- install/databases.js
+++ install/databases.js
@@ -34,6 +34,11 @@ async function getDatabaseConfig(config) {
 			return config;
 		}
 		return await promptGet(questions.mongo);
+	} else if (config.database === 'mongoredis') {
+		if ((config['mongo:host'] && config['mongo:port']) || config['mongo:uri']) {
+			return config;
+		}
+		return await promptGet(questions.mongo);
 	} else if (config.database === 'postgres') {
 		if (config['postgres:host'] && config['postgres:port']) {
 			return config;
@@ -68,13 +73,23 @@ function saveDatabaseConfig(config, databaseConfig) {
 			password: databaseConfig['mongo:password'],
 			database: databaseConfig['mongo:database'],
 			uri: databaseConfig['mongo:uri'],
+		};
+	} else if (config.database === 'mongoredis') {
+		config.mongo = {
+			host: databaseConfig['mongo:host'],
+			port: databaseConfig['mongo:port'],
+			username: databaseConfig['mongo:username'],
+			password: databaseConfig['mongo:password'],
+			database: databaseConfig['mongo:database'],
+			uri: databaseConfig['mongo:uri'],
 		},
 		config.redis = {
 			host: databaseConfig['redis:host'],
 			port: databaseConfig['redis:port'],
 			password: databaseConfig['redis:password'],
 			database: databaseConfig['redis:database'],
-		};
+		},
+		isCluster: true;
 	} else if (config.database === 'postgres') {
 		config.postgres = {
 			host: databaseConfig['postgres:host'],
